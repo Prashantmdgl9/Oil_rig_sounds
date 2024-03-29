478adf6d874ee99ccffee772004d762f32258be0
Prashant Mudgal <Prashantmdgl9@users.noreply.github.com>
2024-03-29 22:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,6 +121,7 @@ def save_file(sound_file):
 
 
 def identify1():
+    set_png_as_page_bg('oil5.png')
 
     st.subheader("Choose an audio file that you extracted from the work site")
     uploaded_file = st.file_uploader('Select')
